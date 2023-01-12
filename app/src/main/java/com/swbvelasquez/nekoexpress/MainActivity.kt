c35307b310f373fe8359d69047ae1b0e82715aa3
swbvelasquez <swbvelasquez@gmail.com>
2023-01-12 14:03:08 -0500

    Initial commit

---
+++ app/src/main/java/com/swbvelasquez/nekoexpress/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.swbvelasquez.nekoexpress
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
